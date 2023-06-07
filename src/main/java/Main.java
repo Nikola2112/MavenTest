0fba52061cb9e0d93ba796199e6630da33a868f6
Nikola2112 <nikola21122004@gmail.com>
2023-06-07 14:33:04 +0100

    new

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.google.gson.Gson;
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Nikolai","Vynohradov");
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,17 @@
+class Person{
+    private String name;
+    private String LastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        LastName = lastName;
+    }
+}
